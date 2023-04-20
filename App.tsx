5ad94f2b9c68103a6be1b3fc8e622864bf93fd26
rsousa86 <rafael.sousa@crdc.com.br>
2023-04-20 20:37:01 -0300

    Instalação e configuração da fonte customizada.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { SafeAreaView } from 'react-native'
+import { Inter_400Regular, Inter_700Bold, useFonts } from '@expo-google-fonts/inter';
+
 import { Home } from './src/screens/Home'
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  });
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Home />
+      { fontsLoaded ? <Home /> : <Loading /> }
     </SafeAreaView>
   )
 }
